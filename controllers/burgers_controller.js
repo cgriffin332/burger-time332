da774abc109bc7840af8e42757efbf9280773eee
Calvin Griffin <cgriffin332@gmail.com>
2020-10-08 22:33:07 -0400

    added get route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,15 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-
-
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    let hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
-
